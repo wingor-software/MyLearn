4dce8d96afc970bb7737434b7d21019ab1ee1361
enshin258 <kuba.win@wp.pl>
2019-10-20 16:41:13 +0200

    porawy w tutorialu

--- app/src/main/java/com/wingor_software/mylearn/tutorial/FragmentTutorial3.java
+++ app/src/main/java/com/wingor_software/mylearn/tutorial/FragmentTutorial3.java
@@ -10,13 +10,13 @@ import androidx.fragment.app.Fragment;
 
 import com.wingor_software.mylearn.R;
 
-public class FragmentTutorial1 extends Fragment {
+public class FragmentTutorial3 extends Fragment {
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_tutorial_1, container, false);
+        View root = inflater.inflate(R.layout.fragment_tutorial_3, container, false);
         return root;
     }
 }
--- app/src/main/java/com/wingor_software/mylearn/tutorial/FragmentTutorial7.java
+++ app/src/main/java/com/wingor_software/mylearn/tutorial/FragmentTutorial7.java
@@ -10,13 +10,13 @@ import androidx.fragment.app.Fragment;
 
 import com.wingor_software.mylearn.R;
 
-public class FragmentTutorial2 extends Fragment {
+public class FragmentTutorial7 extends Fragment {
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_tutorial_2, container, false);
+        View root = inflater.inflate(R.layout.fragment_tutorial_7, container, false);
         return root;
     }
 }
